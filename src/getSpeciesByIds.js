bdc3d3e73fbd5c9d21e12ab9254c180372d58a40
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-16 15:21:27 -0300

    Primeiro Requisito OK

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...ids) {
+  const result = [];
+  ids.forEach((idElement) => {
+    const spicieFind = data.species.find((specie) => specie.id === idElement);
+    result.push(spicieFind);
+  });
+  return result;
 }
 
 module.exports = getSpeciesByIds;
