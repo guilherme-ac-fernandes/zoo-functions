7b718ab17ace392569f4e2041104f31d9be33459
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-16 16:45:28 -0300

    Requisito 4 OK

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,17 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const result = data.employees.filter((element) => (element.managers.includes(managerId)));
+    const resultMap = result.map((element) => `${element.firstName} ${element.lastName}`);
+    return resultMap;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
