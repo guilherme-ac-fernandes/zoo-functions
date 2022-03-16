cd9cb8d3236e63fb23031e949e057530f47fdc30
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-16 17:29:18 -0300

    Requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, specie) => {
+      acc[`${specie.name}`] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.hasOwnProperty('sex')) {
+    return data.species.find((element) => element.name === animal.specie).residents.filter((item) => item.sex === animal.sex).length;
+  }
+  return data.species.find((element) => element.name === animal.specie).residents.length;
 }
+
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
