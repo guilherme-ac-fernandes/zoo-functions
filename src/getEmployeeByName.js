c191ebae870d8a459d688d9116ea832a9cc8dfb3
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-16 16:03:12 -0300

    Requisito 3 OK

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(name) {
+  if (name === undefined) return {};
+  return data.employees.find((item) => (item.firstName === name || item.lastName === name));
 }
 
 module.exports = getEmployeeByName;
