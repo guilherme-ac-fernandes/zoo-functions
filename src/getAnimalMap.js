755ba2f65d998c90283765ef943fcdedcad26141
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-03-16 22:21:14 -0300

    Requisito 7 OK

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -10,51 +10,21 @@ const getRegions = () => data.species.reduce((acc, element) => {
   return acc;
 }, {});
 
-
-
-
-const getRegionWithName = (sort) => data.species.reduce((acc, element) => {
+// Função que criar o mapeamento com filter e sort
+const getRegionWithNameAndSex = (sexAnimal, sorted) => data.species.reduce((acc, element) => {
   if (!acc[element.location]) {
     acc[element.location] = [];
   }
-  const allNames = element.residents.map((item) => item.name);
-  if (sort === true) {
-    allNames.sort();
+  let position = element.residents;
+  if (sexAnimal !== undefined) {
+    position = position.filter((item) => item.sex === sexAnimal);
+  }
+  const allSexNames = position.map((item2) => item2.name);
+  if (sorted === true) {
+    allSexNames.sort();
   }
   // Utilização do objecto entres [] para não causar erro ao atribuir como key do objeto observado a utilização como exemplo no StackOverflow
   // source: https://stackoverflow.com/questions/2274242/how-to-use-a-variable-for-a-key-in-a-javascript-object-literal
-  const animalObject = { [element.name]: allNames };
-  acc[element.location].push(animalObject);
-  return acc;
-}, {});
-
-// const getRegionWithNameSorted = () => data.species.reduce((acc, element) => {
-//   if (!acc[element.location]) {
-//     acc[element.location] = [];
-//   }
-//   const allNames = element.residents.map((item) => item.name).sort();
-//   const animalObject = { [element.name]: allNames };
-//   acc[element.location].push(animalObject);
-//   return acc;
-// }, {});
-
-const getRegionWithNameAndSex = (sexAnimal) => data.species.reduce((acc, element) => {
-  if (!acc[element.location]) {
-    acc[element.location] = [];
-  }
-  const allSexFilter = element.residents.filter((item) => item.sex === sexAnimal);
-  const allSexNames = allSexFilter.map((item2) => item2.name);
-  const animalObject = { [element.name]: allSexNames };
-  acc[element.location].push(animalObject);
-  return acc;
-}, {});
-
-const getRegionWithNameAndSexSorted = (sexAnimal) => data.species.reduce((acc, element) => {
-  if (!acc[element.location]) {
-    acc[element.location] = [];
-  }
-  const allSexFilter = element.residents.filter((item) => item.sex === sexAnimal);
-  const allSexNames = allSexFilter.map((item2) => item2.name).sort();
   const animalObject = { [element.name]: allSexNames };
   acc[element.location].push(animalObject);
   return acc;
@@ -66,17 +36,8 @@ function getAnimalMap(options) {
   const { sex } = options;
   const { sorted } = options;
   console.log(includeNames, sex, sorted);
-  if (includeNames === true && sex !== undefined && sorted === true) return getRegionWithNameAndSexSorted(sex);
-  if (includeNames === true && sex !== undefined) return getRegionWithNameAndSex(sex);
-  if (includeNames && sorted) return getRegionWithName(sorted);
   if (!includeNames) return getRegions();
-  if (includeNames) return getRegionWithName(sorted);
+  return getRegionWithNameAndSex(sex, sorted);
 }
-
-
-
-console.log(getAnimalMap({ includeNames: true, sex: 'female', sorted: true }));
-
-// console.log(getRegionWithNameAndSex('female'));
 
 module.exports = getAnimalMap;
